44fd6c7b56fce167a490691367a4eeea021d9627
Winse <23336098+Winsez@users.noreply.github.com>
2026-08-02 12:57:04 +0300

    day8_schemas

--- backend/day8_schemas.py
+++ backend/day8_schemas.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
@@ -16,3 +16,44 @@ class ToolResponse(BaseModel):
     name: str
     category: str
     status: str
+
+
+tools = []
+
+
+@app.post("/tools", response_model=ToolResponse, status_code=201)
+def post_tool(tool_data: ToolCreate):
+    if tool_data.free:
+        tool_status = "Free"
+    else:
+        tool_status = "Paid"
+
+    tool_id = len(tools) + 1
+
+    new_tool = {
+        "id": tool_id,
+        "name": tool_data.name,
+        "category": tool_data.category,
+        "status": tool_status,
+    }
+
+    tools.append(new_tool)
+
+    return new_tool
+
+
+@app.get("/tools", response_model=list[ToolResponse])
+def get_tools():
+    return tools
+
+
+@app.get("/tools/{tool_id}", response_model=ToolResponse)
+def get_tools_by_id(tool_id: int):
+    for tool in tools:
+        if tool["id"] == tool_id:
+            return tool
+
+    raise HTTPException(
+        status_code=404,
+        detail="Tool not found"
+    )
